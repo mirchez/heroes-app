975acc2ef9408c64c014c624ef2a9f8a39a9d951
Miguel <mmirandasanchez16@gmail.com>
2025-03-11 12:03:41 -0300

    add link to HeroCard

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,55 +1,58 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom"
-import { getHeroById } from "../helpers/getHeroById"
-import { useMemo } from "react"
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
+import { useMemo } from "react";
 
 export const HeroPage = () => {
+  const { id } = useParams();
 
-  const {id} = useParams()
+  const hero = useMemo(() => getHeroById(id), [id]);
 
-  const hero =useMemo(() =>  getHeroById(id), [id])
+  const navigate = useNavigate(); //hook cannot be in conditional functions
 
-  const navigate = useNavigate()//hook cannot be in conditional functions
-
-  const onNavigateBack = () =>{
-      navigate(-1)
-  }
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
-    return <Navigate to="/search"/>//component
+    return <Navigate to="/search" />; //component
   }
 
   return (
     <div className="row mt-5">
+      <div className="col-4 animate__animated animate__fadeInLeft">
+        <img
+          src={`/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail"
+        />
+      </div>
 
-        <div className="col-4 animate__animated animate__fadeInLeft">
-          <img src={`/public/heroes/${id}.jpg`} alt={hero.superhero} className="img-thumbnail"/>
-        </div>
+      <div className="col-8 animate__animated animate__backInRight">
+        <h3>{hero.superhero}</h3>
 
-        <div className="col-8 animate__animated animate__backInRight">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {hero.alter_ego}
+          </li>
 
-          <h3>{hero.superhero}</h3>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {hero.publisher}
+          </li>
 
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item"><b>Alter ego: </b>{hero.alter_ego}</li>
+          <li className="list-group-item">
+            <b>First appearance: </b>
+            {hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
 
-            <li className="list-group-item"><b>Publisher: </b>{hero.publisher}</li>
-            
-            <li className="list-group-item"><b>First appearance: </b>{hero.first_appearance}</li>
-          </ul>
-            <h5 className="mt-3">Characters</h5>
-            <p>{hero.characters}</p>
-
-            <button 
-              className="btn btn-outline-primary"
-              onClick={onNavigateBack}
-            >
-              Last page
-            </button>
-
-        </div>
-
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Last page
+        </button>
+      </div>
     </div>
-  )
-}
-
-
+  );
+};
